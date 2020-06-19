3995910427933cd803b4f653941299f3736ea68c
chen <1074836709@qq.com>
2020-06-19 17:02:21 +0800

    2020-6-19

--- php/admin/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("content-type:text/html;charset=utf-8");
-$conn = new mysqli("localhost", "root", "root123", "myblog");
-if($conn->connect_error){
-  return 0;
-} else {
-  return 1;
-}
